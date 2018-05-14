ff1901392dcca64e067bed6c283c1ac742eb04be
Dennis Stockhofe <scorpion@new-paradigm.de>
2018-05-14 07:34:20 +0200

    Fixes typo

--- src/errors.js
+++ src/errors.js
@@ -1,11 +1,11 @@
 export const actionErrors = {
     NOT_STRING: 'Action Type is not a string',
-    NOT_OBJECT: 'Action is not a object',
+    NOT_OBJECT: 'Action is not an object',
     NOT_SET: 'No Action-Type is set',
     NOT_FOUND: 'Action-Type not found in store'
 };
 
 export const listenerErrors = {
-    NOT_OBJECT: 'No listener is set or is not a object',
+    NOT_OBJECT: 'No listener is set or is not an object',
     NOT_FUNCTION: 'Handler is not a function'
 };
